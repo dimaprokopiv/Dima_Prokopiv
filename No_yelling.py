d562c0b78a0f06a8e8a4bc1e6e18025830e3aa3e
Dima Prokopiv <dimaprokopiv@gmail.com>
2019-06-11 18:02:35 +0300

    No yelling

---
+++ No_yelling.py
@@ -0,0 +1,11 @@
+# Write a function taking in a string like WOW this is REALLY amazing and returning Wow this is really amazing. 
+# String should be capitalized and properly spaced. Using re and string is not allowed.
+
+# Examples:
+
+# filter_words('HELLO CAN YOU HEAR ME') #=> Hello can you hear me
+# filter_words('now THIS is REALLY interesting') #=> Now this is really interesting
+# filter_words('THAT was EXTRAORDINARY!') #=> That was extraordinary!
+
+def filter_words(st):
+    return st[:0].lower() + st[0:].capitalize()
